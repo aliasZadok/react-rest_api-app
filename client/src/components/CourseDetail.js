837e3a0f19a909ba22ad03efcb2845edc84726e5
aliasZadok <lfstone3@gmail.com>
2019-11-05 21:31:27 +0530

    update CourseDetail.js

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -36,7 +36,50 @@ export default class CourseDetail extends Component {
     }
     return(
       <div>
-        
+        <div class="actions--bar">
+          <div class="bounds">
+            <div class="grid-100">
+              {
+                authenticatedUser && authenticatedUser.id === course.userId ?
+                <React.Fragment>
+                  <span>
+                    <Link class="button" to={`courses/${course.id}/update`}>Update Course</Link>
+                    <Link class="button" onClick={this.deleteCourse} to="#">Delete Course</Link>
+                  </span>
+                </React.Fragment> : <React.Fragment> </React.Fragment>
+              }
+              <Link class="button button-secondary" to="/">Return to List</Link>
+            </div>
+          </div>
+        </div>
+        <div class="bounds course--detail">
+          <div class="grid-66">
+            <div class="course--header">
+              <h4 class="course--label">Course</h4>
+              <h3 class="course--title">{course.title}</h3>
+              <p>By {course.userDetails.firstName} {course.userDetails.lastName}</p>
+            </div>
+            <div class="course--description">
+              <ReactMarkdown source={course.description}/>
+            </div>
+          </div>
+          <div class="grid-25 grid-right">
+            <div class="course--stats">
+              <ul class="course--stats--list">
+                <li class="course--stats--list--item">
+                  <h4>Estimated Time</h4>
+                  <h3>{course.estimatedTime}</h3>
+                </li>
+                <li class="course--stats--list--item">
+                  <h4>Materials Needed</h4>
+                  <ul>
+                    <ReactMarkdown source={course.materialsNeeded}/>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
